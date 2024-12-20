0e57448fc80982dda0c983887f4148d87159c282
unknown <2771836756@qq.com>
2024-12-20 19:04:33 +0800

    servlet的生命周期

--- src/main/java/com/opencode/MyServlet.java
+++ src/main/java/com/opencode/MyServlet.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PipedWriter;
+import java.io.PrintWriter;
 
 public class MyServlet extends HttpServlet {
     // 创建 Servlet 的时候进行初始化
@@ -34,11 +36,17 @@ public class MyServlet extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         // 拿参数 http://localhost:8080/myServlet?username="Tom"&password="123456"
         String username = req.getParameter("username");
         String password = req.getParameter("password");
-        System.out.println("账号: " + username);
-        System.out.println("密码: " + password);
+
+        PrintWriter writer = resp.getWriter();
+        // 注意：字符串比较实用 equals() 方法
+        if(username.equals("admin") && password.equals("123456")){
+            writer.println("OK");
+        }else{
+            writer.println("NO");
+        }
     }
 }
